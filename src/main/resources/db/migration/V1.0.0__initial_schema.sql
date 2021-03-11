2e69857ff6dd9590253f6952c0004190a99ef1be
oleg-grigorijan <oleg.grigorijan@gmail.com>
2021-03-11 23:23:57 +0300

    #6: Initial database schema

--- src/main/resources/db/migration/V1.0.0__initial_schema.sql
+++ src/main/resources/db/migration/V1.0.0__initial_schema.sql
@@ -1,33 +1,38 @@
--- TODO: Think about reviewer, employee, brand_presenter
--- TODO: Email verification (tokens)
 -- TODO: Think about images
 -- TODO: Moderator feedback
 -- TODO?: Subject request
 -- TODO?: Administrator report
 -- TODO?: Email tasks
+-- TODO: now() time zone
+-- TODO: Email verification: on update
 
-create table reviewer
+create type actor_role as enum ('REVIEWER', 'HR', 'MODERATOR', 'ADMINISTRATOR', 'BRAND_PRESENTER');
+
+create table actor
 (
-    id                uuid primary key,
-    first_name        varchar(64) not null,
-    last_name         varchar(64) not null,
-    email             varchar(320) not null unique,
-    password          varchar(60) not null,
-    created_timestamp timestamp not null,
-    image_id          uuid
+    id                  uuid primary key,
+    first_name          varchar(64) not null,
+    last_name           varchar(64) not null,
+    email               varchar(320) not null unique,
+    role                actor_role not null,
+    password            varchar(60) not null,
+    created_timestamp   timestamp not null default now(),
+    image_id            uuid
 );
 
-create type employee_role as enum ('HR', 'MODERATOR', 'ADMINISTRATOR');
-
-create table employee
+create table email_verification_token
 (
-    id                uuid primary key,
-    first_name        varchar(64) not null,
-    last_name         varchar(64) not null,
+    token             varchar(73) primary key,
+    actor_id          uuid references actor,
     email             varchar(320) not null unique,
-    password          varchar(60) not null,
-    role              employee_role not null,
-    created_timestamp timestamp not null
+    created_timestamp timestamp not null default now()
+);
+
+create table password_recovery_token
+(
+    token             varchar(73) primary key,
+    actor_id          uuid references actor,
+    created_timestamp timestamp not null default now()
 );
 
 create table brand
@@ -38,30 +43,25 @@ create table brand
     image_id    uuid
 );
 
-create table brand_presenter
+create table brand_presenter_details
 (
-    id                uuid primary key,
-    brand_id          uuid references brand,
-    first_name        varchar(64),
-    last_name         varchar(64),
-    email             varchar(320) not null unique,
-    password          varchar(60),
-    created_timestamp timestamp not null
+    user_id  uuid primary key,
+    brand_id uuid references brand
 );
 
 create table brand_registration_token
 (
-    token             uuid primary key,
+    token             varchar(73) primary key,
     brand_id          uuid not null references brand,
     email             varchar(320) not null,
-    created_timestamp timestamp not null
+    created_timestamp timestamp not null default now()
 );
 
 create table subject
 (
     id          uuid primary key,
     name        varchar(128) not null,
-    description text not null,
+    description varchar not null,
     brand_id    uuid not null references brand
     -- TODO?: images
 );
@@ -82,7 +82,7 @@ create table review
 (
     id          uuid primary key,
     title       varchar(128) not null,
-    reviewer_id uuid not null references reviewer,
+    reviewer_id uuid not null references actor,
     subject_id  uuid not null references subject, -- TODO?: Subjects created by reviewer
     mark        integer not null,
     is_accepted boolean not null,                 -- TODO?: Should it be placed here? Should it be state: ACCEPTED, DECLINED, MODERATING
@@ -93,18 +93,19 @@ create table review_body
 (
     id                uuid primary key,
     review_id         uuid not null references review,
-    content           text not null,
-    created_timestamp timestamp not null
+    content           varchar not null,
+    created_timestamp timestamp not null default now()
 );
 
 create type review_point_type as enum ('ADVANTAGE', 'DISADVANTAGE');
 
 create table review_point
 (
-    review_id uuid primary key references review,
-    ordering  int primary key,
+    review_id uuid references review,
+    ordering  int,
     type      review_point_type not null,
-    content   varchar(256)
+    content   varchar(256),
+    primary key (review_id, ordering)
 );
 
 create table review_image
@@ -118,27 +119,29 @@ create type vote_type as enum ('UP', 'DOWN');
 
 create table review_vote
 (
-    review_id   uuid primary key references review,
-    reviewer_id uuid primary key references reviewer,
-    type        vote_type not null
+    review_id   uuid references review,
+    reviewer_id uuid references actor,
+    type        vote_type not null,
+    primary key (review_id, reviewer_id)
 );
 
 create table review_comment
 (
     id                uuid primary key,
     review_id         uuid not null references review,
-    reviewer_id       uuid references reviewer,
+    reviewer_id       uuid references actor,
     is_brand_response boolean not null,
-    content           text not null,
+    content           varchar not null,
     parent_comment_id uuid references review_comment,
-    created_timestamp timestamp not null
+    created_timestamp timestamp not null default now()
 );
 
 create table review_comment_vote
 (
-    review_comment_id uuid primary key references review_comment,
-    reviewer_id       uuid primary key references reviewer,
-    type              vote_type not null
+    review_comment_id uuid references review_comment,
+    reviewer_id       uuid references actor,
+    type              vote_type not null,
+    primary key (review_comment_id, reviewer_id)
 );
 
 create type report_issuer_type as enum ('GUEST', 'REVIEWER', 'BRAND_OWNER', 'SYSTEM');
@@ -164,12 +167,12 @@ create table moderator_report
     id                      uuid primary key,
     review_id               uuid references review,
     review_comment_id       uuid references review_comment,
-    description             text not null,
-    issuer_reviewer_id      uuid references reviewer,
+    description             varchar not null,
+    issuer_reviewer_id      uuid references actor,
     issuer_type             report_issuer_type not null,
     status                  report_status not null,
-    assignee_moderator_id   uuid references employee,
-    created_timestamp       timestamp not null,
+    assignee_moderator_id   uuid references actor,
+    created_timestamp       timestamp not null default now(),
     last_modified_timestamp timestamp not null,
     check (num_nonnulls(review_id, review_comment_id) = 1)
 );
