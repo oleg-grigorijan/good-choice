4f8878580fb75152c54c549e97e90782f4dc3d12
oleg-grigorijan <oleg.grigorijan@gmail.com>
2021-03-13 13:46:49 +0300

    #6: Initial database schema

--- src/main/resources/db/migration/V1.0.0__initial_schema.sql
+++ src/main/resources/db/migration/V1.0.0__initial_schema.sql
@@ -1,38 +1,36 @@
--- TODO: Think about images
+-- TODO: Checks and triggers
+-- TODO: Think about image storing and referencing
 -- TODO: Moderator feedback
--- TODO?: Subject request
--- TODO?: Administrator report
 -- TODO?: Email tasks
--- TODO: now() time zone
--- TODO: Email verification: on update
 
 create type actor_role as enum ('REVIEWER', 'HR', 'MODERATOR', 'ADMINISTRATOR', 'BRAND_PRESENTER');
 
 create table actor
 (
-    id                  uuid primary key,
-    first_name          varchar(64) not null,
-    last_name           varchar(64) not null,
-    email               varchar(320) not null unique,
-    role                actor_role not null,
-    password            varchar(60) not null,
-    created_timestamp   timestamp not null default now(),
-    image_id            uuid
+    id                 uuid primary key,
+    first_name         varchar(64) not null,
+    last_name          varchar(64) not null,
+    email              varchar(320) not null unique, -- TODO: Email verification on registration, on update
+    role               actor_role not null,
+    password           varchar(60) not null,
+    created_timestamp  timestamp not null,
+    profile_picture_id uuid,                         -- TODO: Images question
+    is_active          boolean not null default true
 );
 
-create table email_verification_token
+create table email_confirmation_token
 (
     token             varchar(73) primary key,
     actor_id          uuid references actor,
     email             varchar(320) not null unique,
-    created_timestamp timestamp not null default now()
+    created_timestamp timestamp not null
 );
 
 create table password_recovery_token
 (
     token             varchar(73) primary key,
     actor_id          uuid references actor,
-    created_timestamp timestamp not null default now()
+    created_timestamp timestamp not null
 );
 
 create table brand
@@ -40,30 +38,34 @@ create table brand
     id          uuid primary key,
     name        varchar(64) not null,
     description text not null,
-    image_id    uuid
+    logo_id     uuid -- TODO: Images question
 );
 
 create table brand_presenter_details
 (
-    user_id  uuid primary key,
+    actor_id uuid primary key references actor,
     brand_id uuid references brand
 );
 
-create table brand_registration_token
+create table brand_invitation_token
 (
     token             varchar(73) primary key,
     brand_id          uuid not null references brand,
     email             varchar(320) not null,
-    created_timestamp timestamp not null default now()
+    created_timestamp timestamp not null
 );
 
+-- TODO?: Subject request
 create table subject
 (
-    id          uuid primary key,
-    name        varchar(128) not null,
-    description varchar not null,
-    brand_id    uuid not null references brand
-    -- TODO?: images
+    id            uuid primary key,
+    name          varchar(128) not null,
+    description   varchar not null,
+    brand_id      uuid not null references brand,
+    is_shown      boolean not null default true,
+    -- TODO?: It will be nice to store more detailed subject summary: count of 5, 4, ..., 0 marks
+    reviews_count int not null default 0,  -- TODO: Trigger
+    average_mark  float not null default 0 -- TODO: Trigger
 );
 
 create table subject_tag
@@ -74,19 +76,22 @@ create table subject_tag
 
 create table subject_to_tag
 (
-    subject_id uuid primary key references subject,
-    tag_id     uuid primary key references subject_tag
+    subject_id uuid references subject,
+    tag_id     uuid references subject_tag,
+    primary key (subject_id, tag_id)
 );
 
 create table review
 (
-    id          uuid primary key,
-    title       varchar(128) not null,
-    reviewer_id uuid not null references actor,
-    subject_id  uuid not null references subject, -- TODO?: Subjects created by reviewer
-    mark        integer not null,
-    is_accepted boolean not null,                 -- TODO?: Should it be placed here? Should it be state: ACCEPTED, DECLINED, MODERATING
-    check (mark >= 0 and mark <= 5)
+    id             uuid primary key,
+    title          varchar(128) not null,
+    reviewer_id    uuid not null references actor,
+    subject_id     uuid not null references subject, -- TODO?: Subjects created by reviewer
+    mark           integer not null,
+    is_shown       boolean not null default true,    -- TODO?: Moderation status: HIDDEN, DECLINED, MODERATING, SHOWN
+    upvotesCount   int not null default 0,           -- TODO: Trigger
+    downvotesCount int not null default 0,           -- TODO: Trigger
+    constraint review_mark_range_check check (mark >= 0 and mark <= 5)
 );
 
 create table review_body
@@ -94,7 +99,7 @@ create table review_body
     id                uuid primary key,
     review_id         uuid not null references review,
     content           varchar not null,
-    created_timestamp timestamp not null default now()
+    created_timestamp timestamp not null
 );
 
 create type review_point_type as enum ('ADVANTAGE', 'DISADVANTAGE');
@@ -104,10 +109,11 @@ create table review_point
     review_id uuid references review,
     ordering  int,
     type      review_point_type not null,
-    content   varchar(256),
+    content   varchar(256) not null,
     primary key (review_id, ordering)
 );
 
+-- TODO: Images question
 create table review_image
 (
     id        uuid primary key,
@@ -129,11 +135,13 @@ create table review_comment
 (
     id                uuid primary key,
     review_id         uuid not null references review,
-    reviewer_id       uuid references actor,
-    is_brand_response boolean not null,
+    author_id         uuid references actor,
     content           varchar not null,
     parent_comment_id uuid references review_comment,
-    created_timestamp timestamp not null default now()
+    created_timestamp timestamp not null,
+    is_shown          bool not null default true, -- TODO?: Moderation status
+    upvotesCount      int not null default 0,     -- TODO: Trigger
+    downvotesCount    int not null default 0      -- TODO: Trigger
 );
 
 create table review_comment_vote
@@ -148,18 +156,20 @@ create type report_issuer_type as enum ('GUEST', 'REVIEWER', 'BRAND_OWNER', 'SYS
 
 create type report_status as enum ('OPEN', 'IN_PROGRESS', 'CLOSED');
 
+-- TODO?: Think is it reasonable solution
 create table moderator_report_reason
 (
     id         uuid primary key,
     name       varchar(128) not null,
     definition varchar(256) not null,
-    is_deleted boolean not null
+    is_shown   boolean not null
 );
 
 create table moderator_report_to_reason
 (
-    report_id uuid primary key references moderator_report,
-    reason_id uuid primary key references moderator_report_reason
+    report_id uuid references moderator_report,
+    reason_id uuid references moderator_report_reason,
+    primary key (report_id, reason_id)
 );
 
 create table moderator_report
@@ -168,11 +178,10 @@ create table moderator_report
     review_id               uuid references review,
     review_comment_id       uuid references review_comment,
     description             varchar not null,
-    issuer_reviewer_id      uuid references actor,
-    issuer_type             report_issuer_type not null,
+    issuer_id               uuid references actor,
     status                  report_status not null,
     assignee_moderator_id   uuid references actor,
-    created_timestamp       timestamp not null default now(),
+    created_timestamp       timestamp not null,
     last_modified_timestamp timestamp not null,
-    check (num_nonnulls(review_id, review_comment_id) = 1)
+    constraint moderator_report_single_target_check check (num_nonnulls(review_id, review_comment_id) = 1)
 );
