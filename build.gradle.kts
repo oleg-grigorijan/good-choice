e7221fd8c8cd48079019e77d8852d8188733e6fb
oleg-grigorijan <oleg.grigorijan@gmail.com>
2021-03-15 21:31:56 +0300

    #23: Setup database migrations with Flyway and jOOQ generation

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,6 @@ dependencies {
     jooqGenerator("org.postgresql:postgresql")
 
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-jooq")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
