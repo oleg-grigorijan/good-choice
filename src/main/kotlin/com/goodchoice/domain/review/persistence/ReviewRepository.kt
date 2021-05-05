5f366218523d75d53c40f091b3ee3e85173d0ab2
AlTsayun <al.tsayun@gmail.com>
2021-05-05 14:19:18 +0300

    #12 implementing review creation

--- src/main/kotlin/com/goodchoice/domain/review/persistence/ReviewRepository.kt
+++ src/main/kotlin/com/goodchoice/domain/review/persistence/ReviewRepository.kt
@@ -1,6 +1,7 @@
 package com.goodchoice.domain.review.persistence
 
 import com.goodchoice.domain.common.jooq.Tables
+import com.goodchoice.domain.common.jooq.enums.ReviewPointType
 import com.goodchoice.domain.common.model.Reference
 import com.goodchoice.domain.review.model.ReviewBodyCreationRequest
 import com.goodchoice.domain.subject.model.Mark
@@ -47,6 +48,24 @@ class ReviewJooqRepository(private val db: DSLContext, private val clock: Clock)
             .set(Tables.REVIEW_BODY.CONTENT, body.content)
             .set(Tables.REVIEW_BODY.CREATED_TIMESTAMP, clock.now())
             .set(Tables.REVIEW_BODY.REVIEW_ID, reviewId)
+            .execute()
+
+        var i: Int = 0
+        db.insertInto(
+            Tables.REVIEW_POINT,
+            Tables.REVIEW_POINT.CONTENT,
+            Tables.REVIEW_POINT.ORDERING,
+            Tables.REVIEW_POINT.REVIEW_ID,
+            Tables.REVIEW_POINT.TYPE
+        ).apply { advantages.forEach { values(it, i++, reviewId, ReviewPointType.ADVANTAGE) } }
+            .execute()
+        db.insertInto(
+            Tables.REVIEW_POINT,
+            Tables.REVIEW_POINT.CONTENT,
+            Tables.REVIEW_POINT.ORDERING,
+            Tables.REVIEW_POINT.REVIEW_ID,
+            Tables.REVIEW_POINT.TYPE
+        ).apply { disadvantages.forEach { values(it, i++, reviewId, ReviewPointType.DISADVANTAGE) } }
             .execute()
 
         return Reference(reviewId)
