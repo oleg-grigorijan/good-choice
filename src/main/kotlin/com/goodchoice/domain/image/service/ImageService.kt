7beada5bd822e1835f4cc96d283a179a09a26afe
AlTsayun <al.tsayun@gmail.com>
2021-05-10 19:35:45 +0300

    numerous fixes

--- src/main/kotlin/com/goodchoice/domain/image/service/ImageService.kt
+++ src/main/kotlin/com/goodchoice/domain/image/service/ImageService.kt
@@ -7,12 +7,15 @@ import com.goodchoice.domain.auth.service.AuthService
 import com.goodchoice.domain.common.model.Reference
 import com.goodchoice.domain.image.model.ImageModificationRequest
 import com.goodchoice.domain.image.persistence.ImageRepository
+import org.springframework.transaction.annotation.Transactional
 
 interface ImageService {
     fun add(request: ImageModificationRequest): Reference
 }
 
 class ImageServiceImpl(private val imageRepo: ImageRepository, private val authService: AuthService) : ImageService {
+
+    @Transactional
     override fun add(request: ImageModificationRequest): Reference {
         authService.currentAuth.requireAnyRole(setOf(REVIEWER, ADMINISTRATOR))
         return imageRepo.add(request.location)
